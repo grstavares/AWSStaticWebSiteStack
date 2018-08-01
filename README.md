fcee5fe03d83d5aba0ca11058f4ba894c5fd50ee
Gustavo Tavares <grstavares@gmail.com>
2018-08-01 00:16:57 -0300

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # AWSStaticWebSiteStack
 Cloud Formation Stack for Static Web Site on AWS
+
+This Stack is used to create all resources needed to create a Static Web Site in AWS. After the execution it will create the folowing resources:
+* S3 Bucket for Static Content;
+* S3 Bucket for Logs;
+* Cloudfront Distribution;
+* Route53 alias for site;
+* AWS Public Certificate.
+
+For proper working you must provide the folowing resources:
+* Hosted Zone configured in AWS Route53;
+* S3 Bucket for Stack resources;
+
+## Intermediate Resources
+During stack creation a few intermediate resources are needed, mainly to automate the certificate approval process. At this moment, three lambda funcionts will be created at your account to perform the certification validation.
+
+## AWS Costs
+The resources created does not incurr costs. You'll have the costs associated with S3 storage for your WebSite objects and CloudFront data transfer costs when your site were active.
+
+### Nexts Steps
+1 - Refactor lambda function to concentrate the three in one.
+2 - Create a CodeCommit repository for static site code;
+3 - Create a CodePipeline Job to build and deploy the code.
---
+++ generate.py
@@ -0,0 +1,95 @@
+#!/usr/local/bin/python
+
+__description__ = 'Python Script to Copy Stack Files to S3 and start the Stack Creation'
+__author__ = 'Gustavo Tavares'
+__version__ = '1.0'
+__date__ = '2018/07/31'
+
+"""
+Source code put in public domain by Gustavo Tavares, no Copyright
+Use at your own risk
+
+History:
+  2018/07/31: start
+
+Todo:
+"""
+
+import boto3
+import argparse
+import os
+import zipfile
+import time
+
+awscliProfile = None
+stackList = ["stacks/master.json", "stacks/functions.json", "stacks/certificate.json", "stacks/website.json", "stacks/pipeline.json"]
+lambdaList = ["lambdas/requestCertificate.js", "lambdas/approveCertificate.js", "lambdas/checkCertificateApproval.js"]
+filesList = stackList + lambdaList
+
+def parseArgs():
+
+    parser = argparse.ArgumentParser("AWS StaticWeb Site Stack Creation")
+    parser.add_argument("-p", "--profile", help="AWS CLI Profile")
+    parser.add_argument("-s", "--stack", help="Stack Name", required=True)
+    parser.add_argument("-b", "--bucket", help="S3 Bucket for Stack Resources (must exist)")
+    parser.add_argument("-z", "--HostedZoneId", help="HostedZone registred in Route53 (must exist)")
+    parser.add_argument("-v", "--verbose", help="Show steps", action="store_true")
+
+    return parser.parse_args()
+
+def checkInputFiles():
+    
+    listChecked = []
+    for file in filesList:
+        listChecked.append(os.path.isfile(file))
+
+    return all(listChecked)
+
+def zipFiles(files):
+
+    zipfiles = []
+    for file in files:
+        zipname = os.path.splitext(file)[0] + ".zip"
+        with zipfile.ZipFile(zipname, 'w') as zipped:
+            zipped.write(file)
+            zipfiles.append(zipname)
+
+    return zipfiles
+
+def getS3():
+    if awscliProfile == None:
+        return boto3.resource('s3')
+    else:
+        session = boto3.Session(profile_name=awscliProfile)
+        return session.resource('s3')
+
+def upload(files, bucket):
+
+    print(files)
+    print(bucket)
+
+    s3 = getS3()
+    for file in files:
+        s3object = s3.Object(bucket, file)
+        s3object.put(Body=open(file, 'rb'))
+
+def clearZipped(files):
+
+    for file in files:
+        os.remove(file)
+
+
+args = parseArgs()
+awscliProfile = args.profile
+
+if not checkInputFiles():
+    raise ValueError('Stack Files not available on current dir!')
+
+bucketName = args.bucket
+zipped = zipFiles(lambdaList)
+upload(stackList + lambdaList, bucketName)
+clearZipped(zipped)
+
+# s3 = getS3()
+# for bucket in s3.buckets.all():
+#    print(bucket.name)
